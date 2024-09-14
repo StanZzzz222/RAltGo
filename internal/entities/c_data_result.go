088bb3d7bc0eb77ee6056af3f1d043a202fca294
StanZzz <2997007973@qq.com>
2024-09-14 16:35:48 +0800

    update c_data_result.go

--- internal/entities/c_data_result.go
+++ internal/entities/c_data_result.go
@@ -46,14 +46,30 @@ func ConverCDataResult(cresPtr uintptr) *CDataResult {
 	if cDataResult == nil {
 		return nil
 	}
-	return &CDataResult{
-		Tag:        Tag(uint8(cDataResult.tag)),
-		U8Val:      uint8(*(*C.uint8_t)(unsafe.Pointer(&cDataResult.data))),
-		U16Val:     uint16(*(*C.uint16_t)(unsafe.Pointer(&cDataResult.data))),
-		U32Val:     uint32(*(*C.uint32_t)(unsafe.Pointer(&cDataResult.data))),
-		U64Val:     uint64(*(*C.uint64_t)(unsafe.Pointer(&cDataResult.data))),
-		BoolVal:    uint64(*(*C.int)(unsafe.Pointer(&cDataResult.data))) != 0,
-		StringVal:  w.PtrMarshalGoString(uintptr(unsafe.Pointer(*(**C.char)(unsafe.Pointer(&cDataResult.data))))),
-		Vector3Val: (*Vector3)(unsafe.Pointer(*(**C.Vector3)(unsafe.Pointer(&cDataResult.data)))),
+	res := &CDataResult{Tag: Tag(uint8(cDataResult.tag))}
+	switch res.Tag {
+	case U8Tag:
+		res.U8Val = uint8(*(*C.uint8_t)(unsafe.Pointer(&cDataResult.data)))
+		break
+	case U16Tag:
+		res.U16Val = uint16(*(*C.uint16_t)(unsafe.Pointer(&cDataResult.data)))
+		break
+	case U32Tag:
+		res.U32Val = uint32(*(*C.uint32_t)(unsafe.Pointer(&cDataResult.data)))
+		break
+	case U64Tag:
+		res.U64Val = uint64(*(*C.uint64_t)(unsafe.Pointer(&cDataResult.data)))
+		break
+	case BoolTag:
+		res.BoolVal = int(*(*C.int)(unsafe.Pointer(&cDataResult.data))) != 0
+		break
+	case StringTag:
+		res.StringVal = w.PtrMarshalGoString(uintptr(unsafe.Pointer(*(**C.char)(unsafe.Pointer(&cDataResult.data)))))
+		break
+	case Vector3Tag:
+		res.Vector3Val = (*Vector3)(unsafe.Pointer(*(**C.Vector3)(unsafe.Pointer(&cDataResult.data))))
+		break
+	default:
 	}
+	return res
 }
