8758ce9df580e118804c326e702c2eb6b46472fc
StanZzz <2997007973@qq.com>
2024-09-24 17:56:02 +0800

    add pools/checkpoint.go and marker_type.go

---
+++ common/alt/pools/checkpoint.go
@@ -0,0 +1,36 @@
+package pools
+
+import "github.com/StanZzzz222/RAltGo/common/models"
+
+/*
+   Create by zyx
+   Date Time: 2024/9/18
+   File: checkpoint.go
+*/
+
+func GetCheckpoint(id uint32) *models.ICheckpoint {
+	return models.GetPools().GetCheckpoint(id)
+}
+
+func GetCheckpoints() []*models.ICheckpoint {
+	var checkpoints []*models.ICheckpoint
+	var pools = models.GetPools().GetColshapePools()
+	pools.Range(func(key, value any) bool {
+		checkpoints = append(checkpoints, value.(*models.ICheckpoint))
+		return true
+	})
+	return checkpoints
+}
+
+func GetCheckpointIterator() <-chan *models.ICheckpoint {
+	var checkpointChan = make(chan *models.ICheckpoint)
+	var pools = models.GetPools().GetColshapePools()
+	go func() {
+		defer close(checkpointChan)
+		pools.Range(func(key, value any) bool {
+			checkpointChan <- value.(*models.ICheckpoint)
+			return true
+		})
+	}()
+	return checkpointChan
+}
---
+++ hash_enums/marker_type/marker_type.go
@@ -0,0 +1,57 @@
+package marker_type
+
+/*
+   Create by zyx
+   Date Time: 2024/9/24
+   File: marker_type.go
+*/
+
+type MarkerType int32
+
+const (
+	MarkerCone MarkerType = iota
+	MarkerCylinder
+	MarkerArrow
+	MarkerArrowFlat
+	MarkerFlag
+	MarkerRingFlag
+	MarkerRing
+	MarkerPlane
+	MarkerBikeLogo1
+	MarkerBikeLogo2
+	MarkerNum0
+	MarkerNum1
+	MarkerNum2
+	MarkerNum3
+	MarkerNum4
+	MarkerNum5
+	MarkerNum6
+	MarkerNum7
+	MarkerNum8
+	MarkerNum9
+	MarkerChevron1
+	MarkerChevron2
+	MarkerChevron3
+	MarkerRingflat
+	MarkerLap
+	MarkerHalo
+	MarkerHaloPoint
+	MarkerHaloRotate
+	MarkerSphere
+	MarkerMoney
+	MarkerLines
+	MarkerBeast
+	MarkerQuestionMark
+	MarkerTransformPlane
+	MarkerTransformHelicopter
+	MarkerTransformBoat
+	MarkerTransformCar
+	MarkerTransformBike
+	MarkerTransformPushBike
+	MarkerTransformTruck
+	MarkerTransformParachute
+	MarkerTransformThruster
+	MarkerWarp
+	MarkerBoxes
+	MarkerPitLane
+)
