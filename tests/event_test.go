f8faecb77e1144e04ef415a7d4f01212bc3f3c45
StanZzz <2997007973@qq.com>
2024-09-30 03:41:26 +0800

    update tests/event_test.go

--- tests/event_test.go
+++ tests/event_test.go
@@ -3,7 +3,7 @@ package tests
 import (
 	"fmt"
 	"github.com/StanZzzz222/RAltGo/common"
-	alt_events2 "github.com/StanZzzz222/RAltGo/common/core/alt/alt_events"
+	"github.com/StanZzzz222/RAltGo/common/core/alt/alt_events"
 	"github.com/StanZzzz222/RAltGo/common/models"
 	"testing"
 )
@@ -17,10 +17,10 @@ import (
 func TestEvent(t *testing.T) {
 	var p = &models.IPlayer{}
 	p = p.NewIPlayer(1, "test", "127.0.0.1", "test", 1, 1, common.NewVector3(0, 0, 0), common.NewVector3(0, 0, 0))
-	alt_events2.Events().OnLocalEvent("test", func(name string, player *models.IPlayer) {
+	alt_events.Events().OnLocalEvent("test", func(name string, player *models.IPlayer) {
 		fmt.Println("Name: ", name)
 		fmt.Println("PlayerId: ", player.GetId())
 	})
 	models.GetPools().PutPlayer(p)
-	alt_events2.EmitLocalEvent("test", "Evan", p)
+	alt_events.EmitLocalEvent("test", "Evan", p)
 }
