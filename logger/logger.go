20cde970c73cd8a258f1cde40a151c7cc4714423
StanZzz <2997007973@qq.com>
2024-09-30 23:43:18 +0800

    update logger/logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"github.com/gookit/color"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,49 +13,78 @@ import (
    File: logger.go
 */
 
-var (
-	successChan = make(chan string)
-	infoChan    = make(chan string)
-	warningChan = make(chan string)
-	errorChan   = make(chan string)
-	printlnChan = make(chan string)
-)
-
-func init() {
-	go func() {
-		for {
-			select {
-			case msg := <-successChan:
-				currentTime := time.Now().Format("15:04:05")
-				color.Print(fmt.Sprintf("[%v]  ", currentTime))
-				color.Rgb(40, 225, 119, false).Println(msg)
-			case msg := <-infoChan:
-				currentTime := time.Now().Format("15:04:05")
-				color.Print(fmt.Sprintf("[%v]  ", currentTime))
-				color.Rgb(49, 122, 221, false).Println(msg)
-			case msg := <-warningChan:
-				currentTime := time.Now().Format("15:04:05")
-				color.Print(fmt.Sprintf("[%v]  ", currentTime))
-				color.Rgb(255, 153, 0, false).Println(msg)
-			case msg := <-errorChan:
-				currentTime := time.Now().Format("15:04:05")
-				color.Print(fmt.Sprintf("[%v]  ", currentTime))
-				color.Rgb(227, 80, 13, false).Println(msg)
-			case msg := <-printlnChan:
-				currentTime := time.Now().Format("15:04:05")
-				color.Println(fmt.Sprintf("[%v]  %v", currentTime, msg))
-			}
-		}
-	}()
+type RAltLogger struct {
+	successChan chan string
+	infoChan    chan string
+	warningChan chan string
+	errorChan   chan string
+	printlnChan chan string
 }
 
-func LogSuccess(msg string)               { successChan <- msg }
-func LogInfo(msg string)                  { infoChan <- msg }
-func LogWarn(msg string)                  { warningChan <- msg }
-func LogError(msg string)                 { errorChan <- msg }
-func LogPrintln(msg string)               { printlnChan <- msg }
-func LogSuccessf(msg string, args ...any) { successChan <- fmt.Sprintf(msg, args...) }
-func LogInfof(msg string, args ...any)    { infoChan <- fmt.Sprintf(msg, args...) }
-func LogWarnf(msg string, args ...any)    { warningChan <- fmt.Sprintf(msg, args...) }
-func LogErrorf(msg string, args ...any)   { errorChan <- fmt.Sprintf(msg, args...) }
-func LogPrintlnf(msg string, args ...any) { printlnChan <- fmt.Sprintf(msg, args...) }
+var (
+	initState   = &atomic.Bool{}
+	successChan chan string
+	infoChan    chan string
+	warningChan chan string
+	errorChan   chan string
+	printlnChan chan string
+)
+
+func Logger() *RAltLogger {
+	if !initState.Load() {
+		successChan = make(chan string)
+		infoChan = make(chan string)
+		warningChan = make(chan string)
+		errorChan = make(chan string)
+		printlnChan = make(chan string)
+		initState.Store(true)
+		go func() {
+			for {
+				select {
+				case msg := <-successChan:
+					currentTime := time.Now().Format("15:04:05")
+					color.Print(fmt.Sprintf("[%v]  ", currentTime))
+					color.Rgb(40, 225, 119, false).Println(msg)
+				case msg := <-infoChan:
+					currentTime := time.Now().Format("15:04:05")
+					color.Print(fmt.Sprintf("[%v]  ", currentTime))
+					color.Rgb(49, 122, 221, false).Println(msg)
+				case msg := <-warningChan:
+					currentTime := time.Now().Format("15:04:05")
+					color.Print(fmt.Sprintf("[%v]  ", currentTime))
+					color.Rgb(255, 153, 0, false).Println(msg)
+				case msg := <-errorChan:
+					currentTime := time.Now().Format("15:04:05")
+					color.Print(fmt.Sprintf("[%v]  ", currentTime))
+					color.Rgb(227, 80, 13, false).Println(msg)
+				case msg := <-printlnChan:
+					currentTime := time.Now().Format("15:04:05")
+					color.Println(fmt.Sprintf("[%v]  %v", currentTime, msg))
+				}
+			}
+		}()
+		return &RAltLogger{successChan, infoChan, warningChan, errorChan, printlnChan}
+	}
+	return &RAltLogger{successChan, infoChan, warningChan, errorChan, printlnChan}
+}
+
+func (logger *RAltLogger) LogSuccess(msg string) { logger.successChan <- msg }
+func (logger *RAltLogger) LogInfo(msg string)    { logger.infoChan <- msg }
+func (logger *RAltLogger) LogWarn(msg string)    { logger.warningChan <- msg }
+func (logger *RAltLogger) LogError(msg string)   { logger.errorChan <- msg }
+func (logger *RAltLogger) LogPrintln(msg string) { logger.printlnChan <- msg }
+func (logger *RAltLogger) LogSuccessf(msg string, args ...any) {
+	logger.successChan <- fmt.Sprintf(msg, args...)
+}
+func (logger *RAltLogger) LogInfof(msg string, args ...any) {
+	logger.infoChan <- fmt.Sprintf(msg, args...)
+}
+func (logger *RAltLogger) LogWarnf(msg string, args ...any) {
+	logger.warningChan <- fmt.Sprintf(msg, args...)
+}
+func (logger *RAltLogger) LogErrorf(msg string, args ...any) {
+	logger.errorChan <- fmt.Sprintf(msg, args...)
+}
+func (logger *RAltLogger) LogPrintlnf(msg string, args ...any) {
+	logger.printlnChan <- fmt.Sprintf(msg, args...)
+}
