5062b5061ef6cca7af0f3b31879004b9df1dc012
StanZzz <2997007973@qq.com>
2024-10-17 00:29:54 +0800

    update retimer/timer/timer.go

--- common/core/retimer/timer/timer.go
+++ common/core/retimer/timer/timer.go
@@ -1,6 +1,9 @@
 package timer
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 /*
    Create by zyx
@@ -9,14 +12,14 @@ import "time"
 */
 
 type ITimer struct {
-	Key        string `json:"key"`
-	NotifyUnix int64  `json:"notify_unix"`
-	Duration   int64  `json:"duration"`
-	Message    string `json:"message"`
-	Expr       string `json:"expr"`
-	Loop       bool   `json:"loop"`
-	LoopCount  int    `json:"loop_count"`
-	IsPause    bool   `json:"stop"`
+	Key        string    `json:"key"`
+	NotifyUnix int64     `json:"notify_unix"`
+	Duration   int64     `json:"duration"`
+	Expr       string    `json:"expr"`
+	Loop       bool      `json:"loop"`
+	LoopCount  int       `json:"loop_count"`
+	IsPause    bool      `json:"stop"`
+	Datas      *sync.Map `json:"datas"`
 }
 
 func NewTimer(key string, duration int64, loop bool, loopCount int) *ITimer {
@@ -24,10 +27,53 @@ func NewTimer(key string, duration int64, loop bool, loopCount int) *ITimer {
 		Key:        key,
 		NotifyUnix: time.Now().Add(time.Duration(duration)).Unix(),
 		Duration:   duration,
-		Message:    "",
 		Expr:       "",
 		Loop:       loop,
 		IsPause:    false,
 		LoopCount:  loopCount,
+		Datas:      &sync.Map{},
 	}
+}
+
+func (t *ITimer) SetData(key string, value any) {
+	t.Datas.Store(key, value)
+}
+
+func (t *ITimer) DelData(key string) {
+	_, ok := t.Datas.Load(key)
+	if ok {
+		t.Datas.Delete(key)
+	}
+}
+
+func (t *ITimer) DelAllData() {
+	t.Datas.Range(func(key, value any) bool {
+		t.Datas.Delete(key)
+		return true
+	})
+}
+
+func (t *ITimer) HasData(key string) bool {
+	_, ok := t.Datas.Load(key)
+	if ok {
+		return true
+	}
+	return false
+}
+
+func (t *ITimer) GetData(key string) any {
+	value, ok := t.Datas.Load(key)
+	if ok {
+		return value
+	}
+	return value
+}
+
+func (t *ITimer) GetDatas() []any {
+	var datas []any
+	t.Datas.Range(func(key, value any) bool {
+		datas = append(datas, key)
+		return true
+	})
+	return datas
 }
